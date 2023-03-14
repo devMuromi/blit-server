e5e0c18d2acb5b2acdb25b531d45b249463395ec
devMuromi <devmuromi@gmail.com>
2023-03-14 17:08:27 +0900

    Refactor image view

--- image/views.py
+++ image/views.py
@@ -1,10 +1,10 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from image.models import Image
 from image.serializers import ImageSerializer
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def image_list(request):
     """
     List all images, or create a new image.
@@ -12,16 +12,16 @@ def image_list(request):
     if request.method == 'GET':
         image = Image.objects.all()
         serializer = ImageSerializer(image, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ImageSerializer(data=data)
+        serializer = ImageSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
 def image_detail(request, pk):
     """
     Retrieve, update or delete an image.
@@ -29,20 +29,19 @@ def image_detail(request, pk):
     try:
         image = Image.objects.get(pk=pk)
     except Image.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=404)
 
     if request.method == 'GET':
         serializer = ImageSerializer(image)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ImageSerializer(image, data=data)
+        serializer = ImageSerializer(image, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         image.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
