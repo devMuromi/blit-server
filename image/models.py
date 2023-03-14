163aa167c22dfcb52cb91c4d414b041484d6d651
devMuromi <devmuromi@gmail.com>
2023-03-14 16:39:07 +0900

    Add Image, User model. pillow library

--- image/models.py
+++ image/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+
+class Image(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+        
--- image/views.py
+++ image/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from image.models import Image
+from image.serializers import ImageSerializer
 
-# Create your views here.
+@csrf_exempt
+def image_list(request):
+    """
+    List all images, or create a new image.
+    """
+    if request.method == 'GET':
+        image = Image.objects.all()
+        serializer = ImageSerialier(image, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ImageSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
