502a394ffc4f576ad56350f28dac20543046177d
devMuromi <devmuromi@gmail.com>
2023-06-13 23:11:21 +0900

    Feat: ocr 앱 일부 수정

--- ocr/views.py
+++ ocr/views.py
@@ -1,10 +1,10 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from receipt.models import Receipt
+from django.core.exceptions import PermissionDenied
 
 from rest_framework.decorators import api_view, permission_classes
 from receipt import permissions as custom_permissions
 from rest_framework import permissions
-
 
 import requests
 import json
@@ -17,26 +17,26 @@ SECRET_KEY = settings.CLOVA_API_KEY
 
 
 @api_view(["GET"])
-@permission_classes([permissions.IsAuthenticated])
 def ocr(request, pk):
-    receipt = Receipt.objects.get(id=pk)
+    try:
+        receipt = Receipt.objects.get(id=pk)
+        if request.user != receipt.uploaded_by:
+            raise PermissionDenied
 
-    if receipt.uploaded_by != request.user:
-        return JsonResponse({"detail": "You do not have permission to perform this action."})
+        if receipt.data == None:
+            image_path = os.path.join(settings.MEDIA_ROOT, str(receipt.image))
+            with open(image_path, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
 
-    if receipt.data == None:
-        image_path = os.path.join(settings.MEDIA_ROOT, str(receipt.image))
-        with open(image_path, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+            raw_data, extracted_data = clova_ocr(encoded_string)
 
-        raw_data, extracted_data = clova_ocr(encoded_string)
+            receipt.raw_data = raw_data
+            receipt.data = extracted_data
+            receipt.save()
 
-        receipt.raw_data = raw_data
-        receipt.data = extracted_data
-        receipt.save()
-
-    # print(receipt.data)
-    return JsonResponse(receipt.data)
+        return JsonResponse(receipt.data)
+    except Receipt.DoesNotExist:
+        raise Http404
 
 
 def clova_ocr(string_encoded_receipt_image: str) -> tuple[dict, dict]:
@@ -44,47 +44,47 @@ def clova_ocr(string_encoded_receipt_image: str) -> tuple[dict, dict]:
         """
         Extract data from ocr result
         return format: {
-            "success": "true",
+            "success": bool_IS_SUCCESS,
             "store": {
-                "name": "name",
-                "address": "address",
+                "name": "str_NAME",
+                "address": "str_ADDRESS",
             },
             "date": {
-                "year": "year",
-                "month": "month",
-                "day": "day",
-                "hour": "hour",
-                "minute": "minute",
+                "year": int_YEAR,
+                "month": int_MONTH,
+                "day": int_DAY,
+                "hour": int_HOUR,
+                "minute": int_MINUTE,
             },
             "items": [
                 {
-                    "name": "name",
-                    "unitPrice": "unitPrice",
-                    "count": "count",
-                    "price": "price",
-                }
+                    "name": "str_NAME",
+                    "unitPrice": int_UNIT_PRICE,
+                    "count": int_COUNT,
+                    "price": int_PRICE,
+                },
             ],
-            "totalPrice": "total",
+            "totalPrice": int_TOTAL_PRICE,
         }
         """
 
         extracted_data = {
-            "success": "true" if data["images"][0]["inferResult"] == "SUCCESS" else "false",
+            "success": True if data["images"][0]["inferResult"] == "SUCCESS" else False,
         }
-        if extracted_data["success"] == "true":
+        if extracted_data["success"] == True:
             data = data["images"][0]["receipt"]["result"]
             extracted_data["date"] = dict()
             extracted_data["items"] = list()
             extracted_data["store"] = dict()
             try:
-                extracted_data["date"]["year"] = data["paymentInfo"]["date"]["formatted"]["year"]
-                extracted_data["date"]["month"] = data["paymentInfo"]["date"]["formatted"]["month"]
-                extracted_data["date"]["day"] = data["paymentInfo"]["date"]["formatted"]["day"]
+                extracted_data["date"]["year"] = int(data["paymentInfo"]["date"]["formatted"]["year"])
+                extracted_data["date"]["month"] = int(data["paymentInfo"]["date"]["formatted"]["month"])
+                extracted_data["date"]["day"] = int(data["paymentInfo"]["date"]["formatted"]["day"])
             except:
                 pass
             try:
-                extracted_data["date"]["hour"] = data["paymentInfo"]["time"]["formatted"]["hour"]
-                extracted_data["date"]["minute"] = data["paymentInfo"]["time"]["formatted"]["minute"]
+                extracted_data["date"]["hour"] = int(data["paymentInfo"]["time"]["formatted"]["hour"])
+                extracted_data["date"]["minute"] = int(data["paymentInfo"]["time"]["formatted"]["minute"])
             except:
                 pass
             try:
@@ -100,19 +100,19 @@ def clova_ocr(string_encoded_receipt_image: str) -> tuple[dict, dict]:
                     extracted_data["items"].append(
                         {
                             "name": item["name"]["formatted"]["value"] if item["name"]["formatted"]["value"] else "null",
-                            "unitPrice": item["price"]["unitPrice"]["formatted"]["value"]
+                            "unitPrice": int(item["price"]["unitPrice"]["formatted"]["value"])
                             if item["price"]["unitPrice"]["formatted"]["value"]
-                            else "null",
-                            "count": item["count"]["formatted"]["value"] if item["count"]["formatted"]["value"] else "null",
-                            "price": item["price"]["price"]["formatted"]["value"]
+                            else None,
+                            "count": int(item["count"]["formatted"]["value"]) if item["count"]["formatted"]["value"] else None,
+                            "price": int(item["price"]["price"]["formatted"]["value"])
                             if item["price"]["price"]["formatted"]["value"]
-                            else "null",
+                            else None,
                         }
                     )
             except:
                 pass
             try:
-                extracted_data["totalPrice"] = data["result"]["totalPrice"]["formatted"]["value"]
+                extracted_data["totalPrice"] = int(data["result"]["totalPrice"]["formatted"]["value"])
             except:
                 pass
 
